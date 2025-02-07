d0abdd625e7bb61ec375694fa1a0e653877412e4
Maaz Amin <maaz@zephyrmonitors.com>
2025-02-07 06:03:06 -0600

    update to chrome 133

--- tls_client/sessions.py
+++ tls_client/sessions.py
@@ -18,7 +18,7 @@ class Session:
 
     def __init__(
         self,
-        client_identifier: ClientIdentifiers = "chrome_131",
+        client_identifier: ClientIdentifiers = "chrome_133",
         ja3_string: Optional[str] = None,
         h2_settings: Optional[Dict[str, int]] = None,
         h2_settings_order: Optional[List[str]] = None,
